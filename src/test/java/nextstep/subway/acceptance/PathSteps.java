a44285a1757f767a30847938bcdd6253b866a348
Yunsik-Choi <choiys0311@gmail.com>
2023-03-06 12:09:24 +0900

    ⛑️ test : 경로 조회 예외 케이스 인수 테스트 작성

--- src/test/java/nextstep/subway/acceptance/PathSteps.java
+++ src/test/java/nextstep/subway/acceptance/PathSteps.java
@@ -19,21 +19,6 @@ public class PathSteps {
         return 경로_찾기_문서화(RestAssured.given(), null, source, target, DISTANCE);
     }
 
-    public static ExtractableResponse<Response> 두_역의_최단_거리_경로_조회를_요청(
-            final Long source,
-            final Long target,
-            final String accessToken
-    ) {
-        return RestAssured.given()
-                .auth().oauth2(accessToken)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .queryParam("source", source)
-                .queryParam("target", target)
-                .queryParam("type", DISTANCE)
-                .when().get("/paths")
-                .then().log().all().extract();
-    }
-
     public static ExtractableResponse<Response> 두_역의_최소_시간_경로_조회를_요청(final Long source, final Long target) {
         return 경로_찾기_문서화(RestAssured.given(), null, source, target, DURATION);
     }
@@ -68,32 +53,16 @@ public class PathSteps {
     public static void 두_역의_최소_시간_경로_조회를_검증(
             final ExtractableResponse<Response> response,
             final Long distance,
-            final Long duration,
-            final int fare
+            final Long duration
     ) {
         Assertions.assertAll(
                 () -> assertThat(response.jsonPath().getLong("distance")).isEqualTo(distance),
-                () -> assertThat(response.jsonPath().getLong("duration")).isEqualTo(duration),
-                () -> assertThat(response.jsonPath().getInt("fare")).isEqualTo(fare)
+                () -> assertThat(response.jsonPath().getLong("duration")).isEqualTo(duration)
         );
     }
 
     public static void 정상_요청이_아닐_경우_예외_처리한다(final ExtractableResponse<Response> findByDistanceResponse) {
         org.assertj.core.api.Assertions.assertThat(findByDistanceResponse.statusCode())
                 .isEqualTo(HttpStatus.BAD_REQUEST.value());
-    }
-
-    public static void 추가_요금이_있는_노선을_이용_할_경우_측정된_요금에_추가한다(
-            final ExtractableResponse<Response> response,
-            final int fare
-    ) {
-        assertThat(response.jsonPath().getInt("fare")).isEqualTo(fare);
-    }
-
-    public static void 추가요금이_있는_노선을_환승_하여_이용_할_경우_가장_높은_금액의_추가_요금만_적용한다(
-            final ExtractableResponse<Response> response,
-            final int fare
-    ) {
-        assertThat(response.jsonPath().getInt("fare")).isEqualTo(fare);
     }
 }
